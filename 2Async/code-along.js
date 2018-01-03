e841700f6e53f9db5d82f9a5d7f408df2cb0076e
mykke <1micheal.sihavong2@gmail.com>
2018-01-03 20:37:44 +0000

    finish CSS, start to async structure

---
+++ 2Async/code-along.js
@@ -0,0 +1,29 @@
+// forEach
+    // format
+        function forEach(array, callback){}
+        function callback(curElement, currentIndex, array){}
+    // non-forEach alternative
+        var arr = [1,2,3,4,5,6];
+        function double(arr){
+            for(var i = 0; i < arr.length; i++){
+                console.log(arr[i] * 2)
+            }
+        }
+        double(arr);
+    // forEach alternative
+        var arr2 = [1,2,3,4,5,6];
+        forEach(arr, function(n){
+            console.log(n * 2)
+        });
+        
+
+// print an array with following words into a sentence: my, forEach, example!!!
+    var strings = ['my', 'forEach', 'example'];
+    var results = '';
+    forEach(strings, function(str, index, array){
+        if (array.length - 1 !== index){
+            results += str + " ";
+        } else {
+            results += str + "!!!";
+        }
+    });
